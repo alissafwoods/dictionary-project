a183bca154073f40ab2f3eac1d23422b4c473ff4
afw <71993863+alissafwoods@users.noreply.github.com>
2022-07-16 15:07:28 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <footer className="App-footer">
+        <p>
+          This project was coded by Alissa Woods and is{" "}
+          <a
+            href="https://github.com/alissafwoods/dictionary-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on Netlify.
+        </p>
+      </footer>
     </div>
   );
 }
