273af4f3c7492bcf198995441d9a1391b4f67f1e
afw <71993863+alissafwoods@users.noreply.github.com>
2022-07-16 20:58:33 -0700

    search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <footer className="App-footer">
-        <p>
-          This project was coded by Alissa Woods and is{" "}
-          <a
-            href="https://github.com/alissafwoods/dictionary-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and hosted on Netlify.
-        </p>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <p>
+            This project was coded by Alissa Woods and is{" "}
+            <a
+              href="https://github.com/alissafwoods/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and hosted on Netlify.
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
